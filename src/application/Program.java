6e6c569478f027a925a573d4b0c4a3c5c1296007
Rafael De Carvalho  Ribeiro <rafael.ribeiro@inmetrics.com.br>
2022-05-31 14:45:40 -0300

    Reusing instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,8 +9,8 @@ public class Program {
 	public static void main(String[] args) {		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(3);
-		
+		System.out.println("=== TEST 1: Seller findById ====");
+		Seller seller = sellerDao.findById(3);		
 		System.out.println(seller);
 
 	}
